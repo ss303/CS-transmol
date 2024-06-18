e133b9998f133d4d9e5a6e0198a1f536c1f30757
Sunny Sun <143143132+ss303@users.noreply.github.com>
2024-06-17 21:41:22 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../.."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../.."))
 # isort: on
 from cerebras.modelzoo.common.utils.run.cli_pytorch import get_params_from_args
 
