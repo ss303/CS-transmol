3aea8568c3d12fedddd14ab6e4d0ad41f202f006
Sunny Sun <143143132+ss303@users.noreply.github.com>
2024-06-17 21:30:34 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,259 @@
+import os
+import torch
+from torch.utils.data import IterableDataset, DataLoader
+import torchtext
+from torchtext.data.utils import RandomShuffler
+from torchtext.data.batch import Batch
+from torchtext import data
+import numpy as np
+
+# Tokenization function for SMILES strings
+def tokenize_smiles(text):
+    return list(text)
+
+# Define constants
+BOS_WORD = '<s>'
+EOS_WORD = '</s>'
+BLANK_WORD = "<blank>"
+MAX_LEN = 100
+MIN_FREQ = 1
+
+# Define Fields for source (SRC) and target (TGT) sequences
+SRC = data.Field(tokenize=tokenize_smiles, pad_token=BLANK_WORD)
+TGT = data.Field(tokenize=tokenize_smiles, init_token=BOS_WORD, eos_token=EOS_WORD, pad_token=BLANK_WORD)
+train_data = None
+val_data = None
+test_data = None
+pad_idx = None
+
+#local_rank = int(os.environ["LOCAL_RANK"])
+#global_rank = torch.distributed.dist.get_rank()
+#device = global_rank % torch.cuda.device_count()
+#torch.cuda.set_device(device)
+
+# Function to load datasets
+def load_datasets(train_file, val_file, test_file):
+    global train_data, val_data, test_data, pad_idx
+    if train_data == None:
+        train_data, val_data, test_data = data.TabularDataset.splits(
+            path='.', 
+            train=train_file,
+            validation=val_file,
+            test=test_file,
+            format='csv',
+            fields=[('src', SRC), ('trg', TGT)],
+            skip_header=False,
+            filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and len(vars(x)['trg']) <= MAX_LEN
+        )
+        # Build vocabularies
+        SRC.build_vocab(train_data.src, min_freq=MIN_FREQ)
+        TGT.build_vocab(train_data.trg, min_freq=MIN_FREQ)
+    if pad_idx == None:
+        pad_idx = TGT.vocab.stoi["<blank>"]
+    
+    return train_data, val_data, test_data
+
+
+class TabularDatasetWrapper(IterableDataset):
+    def __init__(self, tabular_dataset, batch_size=1, repeat=False, batch_size_fn=None, sort_key=None, train=True, sort=None):
+        """
+        Args:
+            tabular_dataset (torchtext.data.TabularDataset): The torchtext TabularDataset.
+        """
+        self.dataset = tabular_dataset
+        self.examples = tabular_dataset.examples
+        self.indices = tabular_dataset.indices
+        self.fields = tabular_dataset.fields
+        self.batch_size = batch_size
+        self.repeat = repeat
+        self.batch_size_fn = batch_size_fn
+        self.sort_key = sort_key
+        self.train = train
+        self.random_shuffler = RandomShuffler()
+        self.sort = not train if sort is None else sort
+        self.iterations = 0
+    
+    def __getitem__(self, i):
+        return self.examples[i]
+    
+    '''
+    def __getitems__(self, indices):
+        return [self.__getitem__(idx) for idx in indices]
+    
+    '''
+
+    def __len__(self):
+        try:
+            return len(self.examples)
+        except TypeError:
+            return 2**32
+    
+    
+    
+    def __iter__(self):
+        self.create_batches()
+        self._iterations_this_epoch = 0
+        for idx, minibatch in enumerate(self.batches):
+                # fast-forward if loaded from state
+                if self._iterations_this_epoch > idx:
+                    continue
+                self.iterations += 1
+                self._iterations_this_epoch += 1
+                if self.sort:
+                    # NOTE: `rnn.pack_padded_sequence` requires that a minibatch
+                    # be sorted by decreasing order, which requires reversing
+                    # relative to typical sort keys
+                    minibatch.reverse()
+                else:
+                    minibatch.sort(key=self.sort_key, reverse=True)
+                yield rebatch(pad_idx, Batch(minibatch, self.dataset, self.device))
+                if not self.repeat:
+                    return
+
+    def __getattr__(self, attr):
+        if attr in self.fields:
+            for x in self.examples:
+                yield getattr(x, attr)
+    
+    def data(self):
+        """Return the examples in the dataset in order, sorted, or shuffled."""
+        if self.sort:
+            xs = sorted(self.dataset, key=self.sort_key)
+        elif self.shuffle:
+            xs = [self.dataset[i] for i in self.random_shuffler(range(len(self.dataset)))]
+        else:
+            xs = self.dataset
+        return xs
+    
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(self.data(), self.random_shuffler)
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size, self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+    
+
+'''
+#write a wrapper class, super class is dataloader. add create_batchs as the 
+class CustomDataLoader(DataLoader):
+    def __init__(self, dataset, batch_size=1, shuffle=False, batch_size_fn=None, sort_key=None, train=True, **kwargs):
+        self.batch_size_fn = batch_size_fn
+        self.sort_key = sort_key
+        self.train = train
+        self.random_shuffler = RandomShuffler()
+        super().__init__(dataset, batch_size, shuffle, **kwargs)
+        self.create_batches(dataset)
+
+    def create_batches(self, data):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(data, self.random_shuffler)
+            self.iter_idx = 0
+        else:
+            self.batches = []
+            for b in data.batch(data, self.batch_size, self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+            self.iter_idx = 0
+    
+    def __iter__(self):
+        batch = self.batches[self.iter_idx]
+        self.iter_idx += 1
+        return batch
+
+class MyIterator(data.Iterator):
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(self.data(), self.random_shuffler)
+            
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size,
+                                          self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+
+'''    
+# Batch size function for padding
+def batch_size_fn(new, count, sofar):
+    global max_src_in_batch, max_tgt_in_batch
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch, len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch, len(new.trg) + 2)
+    src_elements = count * max_src_in_batch
+    tgt_elements = count * max_tgt_in_batch
+    return max(src_elements, tgt_elements)
+
+# Function to rebatch the data to match the input format
+def rebatch(pad_idx, batch):
+    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
+    src, trg = src.cuda(), trg.cuda()
+    return ReBatch(src, trg, pad_idx)
+
+class ReBatch:
+    def __init__(self, src, trg=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if trg is not None:
+            self.trg = trg[:, :-1]
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = self.make_std_mask(self.trg, pad)
+            self.ntokens = (self.trg_y != pad).data.sum()
+
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data)
+        return tgt_mask
+
+def subsequent_mask(size):
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
+
+
+
+
+# Functions to create data loaders as required by Cerebras
+def get_train_dataloader(params):
+    train_data, _, _ = load_datasets(params["train_input"]["train_path"], params["train_input"]["val_path"], params["eval_input"]['eval_path'])
+    train_data_wrapper = TabularDatasetWrapper(train_data, batch_size=params["train_input"]["batch_size"], repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn, train=True)
+    #train_iter = CustomDataLoader(train_data_wrapper, batch_size=params["train_input"]["batch_size"], shuffle=params["train_input"]["shuffle"], sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn, train=True)
+    train_iter = DataLoader(train_data_wrapper, batch_size = None, num_workers = 0, prefetch_factor=None, persistent_workers=None)
+    return train_iter
+
+def get_eval_dataloader(params):
+    _, val_data, _ = load_datasets(params["train_input"]["train_path"], params["train_input"]["val_path"], params["eval_input"]['eval_path'])
+    #val_data_wrapper = TabularDatasetWrapper(val_data)
+    #val_iter = CustomDataLoader(val_data_wrapper, batch_size=params["train_input"]["batch_size"], shuffle=params["train_input"]["shuffle"], sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn, train=False)
+    return #(rebatch(pad_idx, b) for b in val_iter)
+
+
+#from cerebras.modelzoo.common.utils.run.cli_pytorch import get_params_from_args
+#params = {"train_input": {"train_path": "moses_train_small.csv", "val_path": "moses_val_small.csv", "exts": ["src", "trg"], "batch_size": 10, "shuffle": True}, "eval_input": {"eval_path": "moses_val_small.csv"}}
+#dataloader = get_train_dataloader(params)
+#print(dataloader)
+#print((rebatch(pad_idx, b) for b in dataloader))
+#for b in dataloader:
+#    print(b.src, b.trg)
---
+++ run.py
@@ -0,0 +1,75 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../.."))
+# isort: on
+from cerebras.modelzoo.common.utils.run.cli_pytorch import get_params_from_args
+
+def main():
+    params = get_params_from_args()
+    #from cerebras.modelzoo.fc_mnist.pytorch.utils import set_defaults
+
+    #set_defaults(params)
+
+    from cerebras.modelzoo.common.run_utils import main
+    from cerebras.modelzoo.transmol.data import (
+        get_train_dataloader,
+    )
+    dataloader = get_train_dataloader(params)
+    for data in dataloader:
+        print(data)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+'''
+# Load parameters
+with open('configs/params.yaml', 'r') as file:
+    params = yaml.safe_load(file)
+
+# Set random seeds
+torch.manual_seed(params['runconfig']['seed'])
+
+# Load data
+SRC = Field(tokenize=str.split, init_token='<sos>', eos_token='<eos>', lower=True)
+TGT = Field(tokenize=str.split, init_token='<sos>', eos_token='<eos>', lower=True)
+
+train_data, val_data = load_data(params['train_input']['train_path'],
+                                 params['train_input']['val_path'],
+                                 params['train_input']['exts'],
+                                 (SRC, TGT))
+
+train_iter, val_iter = create_iterators(train_data, val_data, 64, torch.device('cuda'))
+
+# Initialize model
+model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
+model = model.cuda()
+
+# Initialize criterion and optimizer
+criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=0, smoothing=0.1)
+criterion = criterion.cuda()
+optimizer = NoamOpt(model.src_embed[0].d_model, 1, 2000,
+                    torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+# Training loop
+for epoch in range(params['runconfig']['max_steps']):
+    model.train()
+    run_epoch((rebatch(0, b) for b in train_iter), model,
+              MultiGPULossCompute(model.generator, criterion, devices=[0], opt=optimizer))
+    model.eval()
+    loss = run_epoch((rebatch(0, b) for b in val_iter), model,
+                     MultiGPULossCompute(model.generator, criterion, devices=[0], opt=None))
+    print(loss)
+    if epoch % params['runconfig']['checkpoint_steps'] == 0:
+        torch.save(model.state_dict(), f'./model/lc_epoch_{epoch}.pt')
+'''
